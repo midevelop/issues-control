6760ebadfaa278b8413d326d61f182bc2e94c115
Mikhail Ivanov <55839915+midevelop@users.noreply.github.com>
2019-11-12 15:23:30 +0300

    build(project): add eslint config

--- js/control.js
+++ js/control.js
@@ -1,4 +1,7 @@
-var app = new Vue({
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable no-return-assign */
+const app = new Vue({
   el: '#app',
   data: {
     username: 'Lyumih',
@@ -6,12 +9,12 @@ var app = new Vue({
     rateLimit: null,
     user: {},
     repos: [],
-    error: ''
+    error: '',
   },
   computed: {
     issuesCount() {
       return this.repos.reduce((sum, repo) => sum + repo.open_issues, 0);
-    }
+    },
   },
   mounted() {
     this.fetchRateLimit();
@@ -25,20 +28,20 @@ var app = new Vue({
     async fetchUser() {
       await axios
         .get(`${this.urlBase}/users/${this.username}`)
-        .then(response => (this.user = response.data))
-        .catch(error => (this.error = error));
+        .then((response) => (this.user = response.data))
+        .catch((error) => (this.error = error));
     },
     async fetchRepos() {
       await axios
         .get(`${this.urlBase}/users/${this.username}/repos`)
-        .then(response => (this.repos = response.data))
-        .catch(error => (this.error = error));
+        .then((response) => (this.repos = response.data))
+        .catch((error) => (this.error = error));
     },
     async fetchRateLimit() {
       await axios
         .get(`${this.urlBase}/rate_limit`)
-        .then(response => (this.rateLimit = response.data))
-        .catch(error => (this.error = error));
-    }
-  }
+        .then((response) => (this.rateLimit = response.data))
+        .catch((error) => (this.error = error));
+    },
+  },
 });
